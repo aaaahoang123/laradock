257756305493c119ac5665d6a5517b9c09fb5295
Hoang Do <hoangdo.2194@gmail.com>
2021-04-14 09:44:03 +0700

    Open xdebug and fix startup

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 if [ -z "$1" ]
-	then docker-compose up nginx mariadb workspace phpmyadmin redis php-fpm mssql elasticsearch zeebe operate php-worker adminer
-	else docker-compose up -$1 nginx mariadb workspace phpmyadmin redis php-fpm mssql elasticsearch zeebe operate php-worker adminer
+	then docker-compose up nginx mariadb workspace phpmyadmin redis php-fpm elasticsearch php-worker adminer
+	else docker-compose up -$1 nginx mariadb workspace phpmyadmin redis php-fpm mssql elasticsearch php-worker adminer
 fi
